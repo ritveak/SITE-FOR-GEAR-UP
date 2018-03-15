6cf95d693dc9f53547778605ad3ed217b8a58ebc
angadsharma1016 <30529572+angadsharma1016@users.noreply.github.com>
2018-03-16 00:56:27 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -8,7 +8,7 @@ module.exports = (from,text)=>{
             service:'gmail',
             auth:{
                 user:secret.email,
-                pass:secret.pass
+                pass:secret.passwd
             }
         });
 
